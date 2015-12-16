c1bdc8f6fb209541b6e9207f0fd463644f0f2b43
Martin Zhou <martin007323@gmail.com>
2015-12-16 09:41:45 +0800

    developing reading part

--- src/main/java/com/lzl/model/LzlModelMain.java
+++ src/main/java/com/lzl/model/LzlModelMain.java
@@ -12,18 +12,52 @@ public class LzlModelMain extends Thread{
 	private MongoClient mongoClient; // MongoDB客户端
 	private MongoDatabase mongoDatabase; //数据库部分
 	/* 可能存在多个Collection ,分管登陆帐号和成绩等 */
-	private MongoCollection<Document> studentAccounts,teacherAccounts,facultyAccounts,managerAccounts;  //students,teachers,facultymen and managers' login accounts
-	private MongoCollection<Document> studentInfo,teacherInfo,courseInfo,scoreInfo,facultyInfo; //students, teachers ,courses, scores and faculotymen's personal information
+
+  	//students,teachers,facultymen and managers' login accounts
+	private MongoCollection<Document> studentAccounts,teacherAccounts,facultyAccounts,managerAccounts;
+
+	//students, teachers ,courses, scores and faculotymen's personal information
+	private MongoCollection<Document> studentInfo,teacherInfo,courseInfo,scoreInfo,facultyInfo; 
+
+	//Communication component to controllor
+	private NetworkTalker talker; 
 	
 	//Initialize the connection to MongoDB
 	private void initializeDatabase(){
 		//默认端口27017
 		mongoClient=new MongoClient("localhost","27017"); 
 		mongoDatabase=mongoClient.getDatabase("stugrademngsys");
+
 		studentAccounts=mongoDatabase.getCollection("studentAccounts");
 		teacherAccounts=mongoDatabase.getCollection("teacherAccounts");
 		facultyAccounts=mongoDatabase.getCollection("facultyAccounts");
 		managerAccounts=mongoDatabase.getCollection("managerAccounts");
-		
+
+		studentInfo=mongoDatabase.getCollection("studentInfo");
+		teacherInfo=mongoDatabase.getCollection("teacherInfo");
+		facultyInfo=mongoDatabase.getCollection("facultyInfo");
+		managerInfo=mongoDatabase.getCollection("managerInfo");
+
+		talker=new NetworkTalker(/*Vport*/,/*Cport*/,/*Mport*/,NetworkTalker.MODEL);
+	}
+
+	//main loop
+	public void run(){
+		while(1){
+			//this program accepts exact one request each time
+			//no more
+			JSONObject request=talker.getNextRequest();
+			String action=(String)request.get("Request");//get to know what action the request want
+			JSONObject firstDetail=(JSONObject)request.get("Detail"); // get the value of first "Detial" key to convenient next stages
+			if(action.equals("ADD")){}
+			else if(action.equals("DEL")){}
+			else if(action.equals("SET")){}
+			else if(action.equals("GET")){
+				String identity=(String)firstDetail.get("Identity");
+				String infotype=(String)firstDetail.get("Infotype");
+				JSONObject secondDetail=(JSONObject)firstDetail.get("Detail");
+				
+			}
+		}
 	}
 }

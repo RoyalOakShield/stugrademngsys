ded6ef7f48738b7c92a36bcdcff1be840c4bc054
Li Ziyi <lostyesterday@163.com>
2015-12-19 13:37:55 +0800

    Part 1

---
+++ src/main/java/com/lzl/controllor/LzlControllerMain.java
@@ -0,0 +1,78 @@
+﻿package com.lzl.controller;
+
+import com.lzl.NetworkTalker;
+import org.json.*;
+import java.io.*;
+
+public class LzlControllerMain extends Thread{
+	private JSONObject reply;
+	private JSONArray replyContents;
+	private int count=0;
+	private String identity;
+	private String infotype;
+
+	private NetworkTalker talker;//Communication component to database & view
+
+	public LzlControllerMain(){//setup controller first then vieW & Model
+		initializeController();
+		new LzlModelMain();
+		new LzlViewmain();
+	}
+
+private	void initializeController{
+	try{
+		talker=new NetworkTalker(6000,6001,6002,NetworkTalker.CONTROLLER);
+	}
+	catch(IOException e){
+		System.err.println("IOException occured while creating NetworkTalker: "+e.toString());
+	}
+}
+
+public void run(){
+	while(true){
+		//single Thread
+		JSONObject requestV=null;//request from view
+		JSONObject requestM=null;//request from model
+		try{
+			requestV=getNextRequest();
+		}catch(IOException e){
+			System.err.println("IOException occured in CONTROLLER while receiving new request: "+e.toString());
+		}
+		//System.out.println(request);//DEBUG!!!!!!!
+
+		String action=(String)request.get("Request");//get to know what action the request want
+		JSONObject firstDetail=(JSONObject)request.get("Detail"); // get the value of first "Detial" key to convenient next stages
+
+		identity=(String)firstDetail.get("Identity");
+		infotype=(String)firstDetail.get("Infotype");
+
+		JSONObject secondDetail=(JSONObject)firstDetail.get("Detail");
+
+		reply=new JSONObject();
+		replyContents=new JSONArray();
+		count=0;
+/*
+*/
+/*+++++login part+++++++++++
+格式{"Request":"SET","Detail":{"Identity":"STUDENT","Infotype
+":"ACCOUNT","Detail":{"Username":"Lizzy","Password"："123456"}}}
+*/
+		if(action.equals("LOGIN")){
+			JSONObject username=identity.get("Username");
+			Password password=;
+			if(passwordV=passwordM){
+				return true;
+			}
+		}
+		else{
+			talk.sendRequest(NetworkTalker.Model,action);
+			talk.sendRequest(NetworkTalker.Model,firstDetail);
+			talk.sendRequest(NetworkTalker.Model,identity);
+			talk.sendRequest(NetworkTalker.Model,infotype);
+			talk.sendRequest(NetworkTalker.Model,secondDetail);
+		}
+		//semd back to view
+
+	}
+
+}
